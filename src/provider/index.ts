9454bbc55eeff34014c90cf48777912f719bf071
gcerdas16 <gcerdas16@gmail.com>
2025-03-10 22:56:56 -0600

    3rd commit

--- src/provider/index.ts
+++ src/provider/index.ts
@@ -1,10 +1,15 @@
-import { MetaProvider as Provider } from "@builderbot/provider-meta";
-import { createProvider } from "@builderbot/bot";
-import { config } from "../config";
+import { createProvider } from '@builderbot/bot';
+import { MetaProvider } from '@builderbot/provider-meta';
+import { BaileysProvider } from '@builderbot/provider-baileys';
+import { config } from '../config/index.js';
 
-export const provider = createProvider(Provider, {
+const providerMeta = createProvider(MetaProvider, {
   jwtToken: config.jwtToken,
   numberId: config.numberId,
   verifyToken: config.verifyToken,
   version: config.version,
 });
+
+const providerBaileys = createProvider(BaileysProvider);
+
+export { providerMeta, providerBaileys };
